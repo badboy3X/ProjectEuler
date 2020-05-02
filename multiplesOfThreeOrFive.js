0c20282d95240c07996211e816075b84217314b5
vishnudeviravi <ivishnudr@gmail.com>
2020-05-03 00:30:38 +0530

    Multiples of 3 and 5

---
+++ multiplesOfThreeOrFive.js
@@ -0,0 +1,11 @@
+const mutiplesOfThreeOrFive = number =>{
+	let sum = 0;
+	for (i = 1; i < 1000; i++) {
+		if (i%3 === 0 || i%5 === 0) {
+			sum += i;
+		};
+	};
+	return sum;
+};
+
+console.log(mutiplesOfThreeOrFive(1000));
